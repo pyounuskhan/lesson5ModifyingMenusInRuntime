b38856d8927f6bb8c180c1c7c46a73fe1051e37d
pyounuskhan <pyounuskhan@gmail.com>
2018-03-08 21:25:15 +0530

    Initial commit

---
+++ app/src/main/java/ifirst/co/lesson5modifyingmenusinruntime/MainActivity.java
@@ -0,0 +1,54 @@
+package ifirst.co.lesson5modifyingmenusinruntime;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int MENU_DOWNLOAD = 1;
+    private final int MENU_SETTINGS = 2;
+    private boolean showDownloadMenu = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void toggleMenu(View view) {
+        showDownloadMenu=!showDownloadMenu;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, MENU_DOWNLOAD, 0, R.string.menu_download);
+        menu.add(0, MENU_SETTINGS, 0, R.string.menu_settings);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem menuItem = menu.findItem(MENU_DOWNLOAD);
+        menuItem.setVisible(showDownloadMenu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_DOWNLOAD:
+                Toast.makeText(this, R.string.menu_download, Toast.LENGTH_LONG).show();
+                break;
+            case MENU_SETTINGS:
+                Toast.makeText(this, R.string.menu_settings, Toast.LENGTH_LONG).show();
+                break;
+            default:
+                return super.onContextItemSelected(item);
+        }
+        return true;
+    }
+}
